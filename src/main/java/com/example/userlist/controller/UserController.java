20ae81eba969ddf6b223b879056cdec8e9861bdd
oluwayhemisi <ismailoluwayemisi@gmail.com>
2023-12-05 16:13:27 +0100

    "first commit"

--- src/main/java/com/example/userlist/controller/UserController.java
+++ src/main/java/com/example/userlist/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/auth/customer")
+@RequestMapping("/api/v1/users")
 @AllArgsConstructor
 public class UserController {
     private final UserService userService;
